9849738016c5f4023a02d9f58b204d43695e5507
dsanchezsantos <dsanchezsantos@gmail.com>
2025-02-07 19:43:55 -0300

    change the filters

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
+import locale
 import plotly.express as px
 from supabase import create_client
-from datetime import datetime
+from datetime import datetime as dt
+
+locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
 
 SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmaHl1dmppcW9zZHJ6Y3FneHBhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMTQ1NjIwNywiZXhwIjoyMDE3MDMyMjA3fQ.EvNKry-AIdhfq-SZW6lrhEM0716nRDd-KLCxqhn6Y1s"
 SUPABASE_URL="https://vfhyuvjiqosdrzcqgxpa.supabase.co"
@@ -37,15 +40,18 @@ df["Dia da semana"] = df["data_saida"].dt.day_name()
 df["ano"] = df["data_saida"].dt.year
 df["mes"] = df["data_saida"].dt.month
 
-# Sidebar - Filtros de ano e mês
-ano_selecionado = st.sidebar.selectbox("Selecione o ano", sorted(df["ano"].unique()))
-mes_selecionado = st.sidebar.selectbox("Selecione o mês", range(1, 13))
+# Gráfico de histogramas
+st.title("Análise de Planos de Navegação")
+
+col1, col2 = st.columns(2)
+
+# Filtros de ano e mês
+ano_selecionado = col1.selectbox("Selecione o ano", sorted(df["ano"].unique()))
+mes_selecionado = col2.selectbox("Selecione o mês", range(1, 13))
 
 # Filtrar dados com base nos seletores
 df_filtrado = df[(df["ano"] == ano_selecionado) & (df["mes"] == mes_selecionado)]
 
-# Gráfico de histogramas
-st.title("Análise de Planos de Navegação")
 st.subheader("Distribuição por Dia da Semana")
 fig1 = px.histogram(df_filtrado, x="Dia da semana", title="Quantidade de Registros por Dia da Semana")
 st.plotly_chart(fig1)
